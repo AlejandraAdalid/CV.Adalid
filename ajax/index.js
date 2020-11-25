889a755487afa9d43e2ca3a67d7c518ec159af30
AlejandraAdalid <63122865+AlejandraAdalid@users.noreply.github.com>
2020-11-25 14:22:51 -0600

    Add files via upload

---
+++ ajax/index.js
@@ -0,0 +1,21 @@
+const Estudios = [{
+   Puesto: "Administrativa" ,
+    Rubro : "Comercio/ Hardware -Computadoras" ,
+    Lugar : "Dorrego 069, Rosario" ,
+    Fecha : "2017-2019",
+},
+{
+    Puesto: "Encargada de Salón y Cocina" ,
+    Rubro : "Restaurante “La Pirucha” " ,
+    Lugar : "Cafferata 1197, Rosario" ,
+    Fecha : "2016-2017",
+},
+{
+    Puesto: "Enseñanza" ,
+    Rubro : "Plataforma educativa " ,
+    Lugar : "Online" ,
+    Fecha : "2014-2015",
+},
+
+];
+
